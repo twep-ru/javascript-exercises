be88605f23534ab92b2549c504bf5b0624f20bee
kagulion <kagulion@vk.com>
2025-07-11 09:41:53 +0300

    Translate Exercise 04 to Russian

--- 04_mathEquations/mathEquations.spec.js
+++ 04_mathEquations/mathEquations.spec.js
@@ -1,22 +1,22 @@
 const {a, b, c, d, e, f} = require('./mathEquations')
 
 describe('math', () => {
-  test('a is 0', () => {
+  test('a это 0', () => {
     expect(a).toEqual(0);
   });
-  test.skip('b is 9', () => {
+  test.skip('b это 9', () => {
     expect(b).toEqual(9);
   });
-  test.skip('c is 66', () => {
+  test.skip('c это 66', () => {
     expect(c).toEqual(66);
   });
-  test.skip('d is 1', () => {
+  test.skip('d это 1', () => {
     expect(d).toEqual(1);
   });
-  test.skip('e is -8', () => {
+  test.skip('e это -8', () => {
     expect(e).toEqual(-8);
   });
-  test.skip('f is 68', () => {
+  test.skip('f это 68', () => {
     expect(f).toEqual(68);
   });
 });
--- 04_mathEquations/solution/mathEquations-solution.spec.js
+++ 04_mathEquations/solution/mathEquations-solution.spec.js
@@ -1,22 +1,22 @@
 const {a, b, c, d, e, f} = require('./mathEquations-solution')
 
 describe('math', () => {
-  test('a is 0', () => {
+  test('a это 0', () => {
     expect(a).toEqual(0);
   });
-  test('b is 9', () => {
+  test('b это 9', () => {
     expect(b).toEqual(9);
   });
-  test('c is 66', () => {
+  test('c это 66', () => {
     expect(c).toEqual(66);
   });
-  test('d is 1', () => {
+  test('d это 1', () => {
     expect(d).toEqual(1);
   });
-  test('e is -8', () => {
+  test('e это -8', () => {
     expect(e).toEqual(-8);
   });
-  test('f is 68', () => {
+  test('f это 68', () => {
     expect(f).toEqual(68);
   });
 });
