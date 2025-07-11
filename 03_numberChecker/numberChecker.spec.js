4e10bdde4ea497d4f888b521f9c5160ad79963e2
kagulion <kagulion@vk.com>
2025-07-11 09:41:06 +0300

    Translate Exercise 03 to Russian

--- 03_numberChecker/numberChecker.spec.js
+++ 03_numberChecker/numberChecker.spec.js
@@ -1,16 +1,16 @@
 const numberChecker = require('./numberChecker')
 
 describe('numberChecker', () => {
-  test('returns true when number is 1000', () => {
+  test('возвращает true, когда число 1000', () => {
     expect(numberChecker(1000)).toEqual(true);
   });
-  test.skip('returns true when number is 10', () => {
+  test.skip('возвращает true, когда число 10', () => {
     expect(numberChecker(10)).toEqual(true);
   });
-  test.skip('returns false when number is 9', () => {
+  test.skip('возвращает false, когда число 9', () => {
     expect(numberChecker(9)).toEqual(false);
   });
-  test.skip('returns false when number is 6', () => {
+  test.skip('возвращает false, когда число 6', () => {
     expect(numberChecker(6)).toEqual(false);
   });
 });
